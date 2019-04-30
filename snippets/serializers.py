b07c40ec1ec24dc23db839a124445e6f7b57e66e
pdw <devdw98@gmail.com>
2019-04-30 19:44:50 +0900

    Tutorial 1-2 Using ModelSerializers ~

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,12 +3,9 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+    class Meta:
+        model = Snippet #Serializer class의 단축버전
+        field = ('id','title','code','linenos','language','style')
 
     def create(self, validated_data):
         """
@@ -30,39 +27,8 @@ class SnippetSerializer(serializers.Serializer):
 
     """
     python manage.py shell
->>> from snippets.models import Snippet
 >>> from snippets.serializers import SnippetSerializer
->>> from rest_framework.renderers import JSONRenderer
->>> from rest_framework.parsers import JSONParser
-
-* snippet 인스턴스 만들기
->>> snippet = Snippet(code='foo = "bar"\n')
->>> snippet.save()
->>> snippet = Snippet(code='print "hello, world"\n')
->>> snippet.save()
-* 인스턴스 중 하나 직렬화(serializer) 하기 - 파이썬의 데이터 타입으로 변환된다.
->>> serializer = SnippetSerializer(snippet)
->>> serializer.data
-{'pk': 2, 'title': '', 'code': 'print "hello, world"\n', 'linenos': False, 'language': 'python', 'style': 'friendly'}
-* 직렬화 과정 마무리하기 - json으로 변환한다.
->>> content = JSONRenderer().render(serializer.data)
->>> content
-b'{"pk":2,"title":"","code":"print \\"hello, world\\"\\n","linenos":false,"language":"python","style":"friendly"}'
-* 반직렬화 하기 - 파이썬의 데이터 타입을 분석(parse)한다.
->>> from django.utils.six import BytesIO
->>> stream = BytesIO(content)
->>> data = JSONParser().parse(stream)
-* 반직렬화 마무리 - 데이터를 인스턴스화 한다.
->>> serializer = SnippetSerializer(data=data)
->>> serializer.is_valid()
-True
->>> serializer.validated_data
-OrderedDict([('title', ''), ('code', 'print "hello, world"'), ('linenos', False), ('language', 'python'), ('style', 'friendly')])
->>> serializer.save()
-<Snippet: Snippet object (3)>
-* 쿼리셋의 직렬화 - many=True 추가하기
->>> serializer = SnippetSerializer(Snippet.objects.all(), many=True)
->>> serializer.data
-[OrderedDict([('pk', 1), ('title', ''), ('code', 'foo = "bar"\n'), ('linenos', False), ('language', 'python'), ('style', 'friendly')]), OrderedDict([('pk', 2), ('title', ''), ('code', 'print "hello, world"\n'), ('linenos', False),
-('language', 'python'), ('style', 'friendly')]), OrderedDict([('pk', 3), ('title', ''), ('code', 'print "hello, world"'), ('linenos', False), ('language', 'python'), ('style', 'friendly')])]
+>>> serializer = SnippetSerializer()
+>>> print(repr(serializer))
+SnippetSerializer():
     """
--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+#새로 만든 serializer class를 view에서 어떻게 사용하는가!
+#django view형태
+
+"""
+class JSONResponse(HttpResponse):
+    
+    #콘텐츠를 JSON으로 변환한 후 HttpResponse 형태로 반환한다.
+    
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+"""
+@csrf_exempt #인증되지 않은 사용자도 POST할 수 있도록 한다.
+def snippet_list(request):
+    """
+    코드 조각을 모두 보여주거나 새 코드 조각을 만듭니다.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JSONResponse(serializer.data)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data,status=201)
+        return JSONResponse(serializer.error,status=400)
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    코드 조각 조회, 업데이트, 삭제
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExsit:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+    
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return JSONResponse(status=204)
