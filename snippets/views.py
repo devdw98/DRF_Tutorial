2ec8ad96a99cb924123aa16b9b95b1e35872a8db
pdw <devdw98@gmail.com>
2019-04-30 20:01:06 +0900

    Tutorial 2 Requests and Responses

--- snippets/views.py
+++ snippets/views.py
@@ -1,63 +1,52 @@
-from django.http import HttpResponse,JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 #새로 만든 serializer class를 view에서 어떻게 사용하는가!
-#django view형태
+#Rest_Framework view형태
+#request.data는 json요청 뿐만 아니라 다른 포맷도 다룰 수 있다.
+#>> 우리가 원하는 형태의 응답 객체를 렌더링해준다.
 
-"""
-class JSONResponse(HttpResponse):
-    
-    #콘텐츠를 JSON으로 변환한 후 HttpResponse 형태로 반환한다.
-    
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-"""
-@csrf_exempt #인증되지 않은 사용자도 POST할 수 있도록 한다.
-def snippet_list(request):
+@api_view(['GET','POST'])
+def snippet_list(request,format=None):
     """
     코드 조각을 모두 보여주거나 새 코드 조각을 만듭니다.
     """
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
     
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data,status=201)
-        return JSONResponse(serializer.error,status=400)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return JSONResponse(serializer.error,status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def snippet_detail(request, pk):
+@api_view
+def snippet_detail(request, pk, format=None):
     """
     코드 조각 조회, 업데이트, 삭제
     """
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExsit:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = SnippetSerializer(snippet)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
+        serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     elif request.method == 'DELETE':
         snippet.delete()
-        return JSONResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
